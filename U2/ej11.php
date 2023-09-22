c96960094d415ee33d68dda45c49be83b55a2e7e
Manuel Bote <mbotez01@educarex.es>
2023-09-22 13:13:25 +0200

    v2

--- U2/ej11.php
+++ U2/ej11.php
@@ -6,6 +6,20 @@
     <title>Ej11</title>
 </head>
 <body>
+
+    <?php
+
+        $array=array();
+
+        for($num=0;$num<11;$num++){
+            $array[$num]=$num**2;
+        }
+
+        foreach($array as $x=>$total){
+            echo $x.'^2: '.$total.'<br/>';
+        }
+
+    ?>
     
 </body>
 </html>
