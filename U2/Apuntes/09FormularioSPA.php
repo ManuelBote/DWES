0bf469bd2a5db55bb4390b8ed24fd2755fa2ff9a
Manuel Bote <mbotez01@educarex.es>
2023-09-26 11:14:17 +0200

    09

---
+++ U2/Apuntes/09FormularioSPA.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="" method="post">
+
+    <div>
+        <label>Nombre</label>
+        <input type="text">
+    </div>
+    <div>
+        <label>Nº de acompañantes</label>
+        <input type="number" value="0">
+    </div>
+    <input type="submit" value="rellenar">
+
+
+    </form>
+
+</body>
+</html>
