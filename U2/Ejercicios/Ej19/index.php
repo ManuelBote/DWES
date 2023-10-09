569baeb22fb093efcd2f0c8c64258fa4ea0d7c3a
Manuel Bote <mbotez01@educarex.es>
2023-10-09 10:25:49 +0200

    Ej 19 terminado

--- U2/Ejercicios/Ej19/index.php
+++ U2/Ejercicios/Ej19/index.php
@@ -14,28 +14,48 @@
         <input type="submit" name="rellenar" value="Rellenar Opciones">
 
         <?php
-            if(isset($_POST['rellenar'])){
+            if(isset($_POST['rellenar']) or isset($_POST['mostrar'])){
         ?>
         <hr>
         <div>
             <label for="color1">Color de Fondo</label>
-            <input type="color" name="colorF">
+            <input type="color" name="colorF" 
+            value="<?php echo (isset($_POST['colorF'])?$_POST['colorF']:"")?>">
         </div>
         <div>
             <label for="color2">Color de Texto</label>
-            <input type="color" name="colorT">
+            <input type="color" name="colorT"
+            value="<?php echo (isset($_POST['colorT'])?$_POST['colorT']:"")?>">
         </div>
         <br>
         Rellenar Opciones
         <br>
         <?php
             for($i=0;$i<$_POST['numero'];$i++){
-                echo '<br><input type="text" name="opciones[]" placeholder="Opcion: '.($i+1).'">';
+                echo '<br><input type="text" name="opciones[]" placeholder="Opcion: '.($i+1).'"
+                value="'.(isset($_POST['opciones'][$i])?$_POST['opciones'][$i]:"").'">';
             }
         }
         ?>
+        <br><br>
+        <input type="submit" name="mostrar" value="Mostrar Menu">
 
     </form>
+    <hr>
+
+    <!-- MENU-->
+    <?php
+
+        if(isset($_POST['mostrar'])){
+            if(isset($_POST['opciones'])){
+                foreach($_POST['opciones'] as $opcion){
+                    echo '<span style="background-color:'.$_POST['colorF'].'; 
+                    color:'.$_POST['colorT'].'; margin:5px; padding:5px">'.$opcion.'</span>';
+                }
+            }
+        }
+
+    ?>
    
 </body>
 </html>
